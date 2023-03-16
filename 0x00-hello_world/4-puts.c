5d266c7a36e8b535d9d7f0e7186f4da80b6cea9f
emilyakoth <emilyakoth9@gmail.com>
2023-03-16 15:45:51 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - printthe string in the put function
+ * main - print the string in the put function
  *
  * Description: use the main function
  * this program prints "programming is like a multilingual puzzl
- * Return : 0
+ * Return : Always 0 (Sucess)
  */
 int main(void)
 {
