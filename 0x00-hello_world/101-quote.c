ee6840331cf81fec515b52ce75c871886f7b5abd
emilyakoth <emilyakoth9@gmail.com>
2023-03-17 03:18:10 -0700

    hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * prints string to stdout
+ * Return: Always 1 if error
+ */
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+	return (1);
+}
