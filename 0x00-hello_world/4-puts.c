8dbb4ad40414c180b115080a42d48e1ba9b90bd1
emilyakoth <emilyakoth9@gmail.com>
2023-03-17 01:46:19 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * Description: main - prints out a string to stdout.
- * this program prints "programming is like a multilingual puzzl
- * Return 0 if success.
+ * main - Entry point
+ * this program prints programming is like a multilingual puzzl
+ * Return: Always 0 if sucess
  */
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
