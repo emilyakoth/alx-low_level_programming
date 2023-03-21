1668e53c3bf9fab4b3d827e606d0b60944ea1198
emilyakoth <emilyakoth9@gmail.com>
2023-03-21 11:29:32 -0700

    add values

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,18 +2,20 @@
 
 /**
  * main - Entry point
+ *
  * prints putchar
+ *
  * Return: Always 0 if success
  */
+
 void print_alphabet(void)
 {
-	char ch;
+	char c;
 
-	for (c = 'a'; c <= 'z'; ch++);
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
 	}
 
 	_putchar('\n');
-	return (0);
 }
