72e3c9791877e37a646e017105c1244af7a30cb6
emilyakoth <emilyakoth9@gmail.com>
2023-03-22 02:14:07 -0700

    commit changes

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,15 +2,14 @@
 
 /**
   * _islower - Checks for lowercase character
-  * @c: The character to be checked
-  *
+  i*
   * Return: 1 for lowercase character or 0 for anything else
   */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
-	{
+	
 		return (1);
-	}
+	if else
 	return (0);
 }
