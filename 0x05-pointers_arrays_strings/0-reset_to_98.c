343d54a167380ba597914c7a2c9689780abd29ec
emilyakoth <emilyakoth9@gmail.com>
2023-03-27 06:52:33 -0700

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,17 +1,11 @@
 #include "main.h"
 
 /**
- * main - entry point
  * reset_to_98 - points parameter
- * @*n - pointer
+ * @n - pointer
  * Return: void
  */
 void reset_to_98(int *n)
 {
-	int n;
-	int *p;
-
-	n = 98;
-	p = &n
+		*n = 98;
 }
-
