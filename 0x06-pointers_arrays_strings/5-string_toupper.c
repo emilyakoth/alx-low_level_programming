3cb4ab1b1de4f118f78f544a0d9f6e0e14c608d7
emilyakoth <emilyakoth9@gmail.com>
2023-03-29 09:05:44 -0700

    print strings

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase to uppercase
+ * @n: the string
+ * Return: char
+ */
+char *string_toupper(char *)
+{
+	int a = 0;
+
+	while (n[a])
+	{
+		if (n[a] >= 97 && n[a] <= 122)
+		{
+			n[a] -= 32
+		}
+		a++;
+	}
+
+	return (n);
+}
