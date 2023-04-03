241a4d128507531e5d19a1c4609601093879f132
emilyakoth <emilyakoth9@gmail.com>
2023-04-03 11:48:05 -0700

    fill memory

--- 0-memset.c
+++ 0-memset.c
@@ -1,1 +1,20 @@
 #include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: memory area
+ * @b: constatnt byte
+ * @n: number of bytes
+ * Return: memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x <= n; x++)
+	{
+		s[x] = b;
+	}
+
+	return (s);
+}
