1c8c9ec0aadc943d40760ce8bbba27a66561c621
emilyakoth <emilyakoth9@gmail.com>
2023-04-03 12:47:39 -0700

    fill memory

---
+++ 1-memcpy.c
@@ -0,0 +1,20 @@
+#includ "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: memory area to copy to
+ * @src: memory area to copy from
+ * @n: number of bytes copied
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i]
+	}
+
+	return (dest);
+}
