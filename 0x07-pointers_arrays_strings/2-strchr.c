93647387302a752e5db317181aa23be601f0e4ea
emilyakoth <emilyakoth9@gmail.com>
2023-04-03 23:14:48 -0700

    locate character in string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character
+ * Return:the string
+ */
+char *_strchr(char *s, char c)
+{
+	while (s != '\0')
+	{
+		if (s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
