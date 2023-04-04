4e5187cfc98c54048de3474e7cc34bbb4b3e1d7a
emilyakoth <emilyakoth9@gmail.com>
2023-04-03 23:54:37 -0700

    print chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,16 +8,15 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
-
-	for (i = 0; i < 8; i++)
+	int b, c;
+	
+	for (b = 0; b < 8; b++)
 	{
-		for (j = 0; j < 8; j++)
+		for (c = 0; c < 8; c++)
 		{
-			_putchar(a[i][j]);
+			_putchar(a[b][c]);
 		}
 
 		_putchar('\n');
 	}
 }
-
