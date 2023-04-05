c92348f04ee8d22a46f98215c60b2909dc49bd89
emilyakoth <emilyakoth9@gmail.com>
2023-04-04 19:24:08 -0700

    length of string

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/**  _strspn - gets lenght of a substring
+/**
+ * _strspn - gets lenght of a substring
  * @s: source string
  * @accept: accepted string
  * Return: bytes
@@ -10,7 +11,7 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int a = 0;
 	unsigned int b;
 	unsigned int t = 0;
-	
+
 	while (accept[a])
 	{
 		b = 0;
