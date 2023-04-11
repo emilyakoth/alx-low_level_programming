2e8bd83710a7b1ebd2de9fe551a7fc3d739ce307
emilyakoth <emilyakoth9@gmail.com>
2023-04-10 19:32:04 -0700

    print coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,12 +10,12 @@
  *
  * Return: 0
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int num, j, result;
 	int coins[] = {25, 10, 5, 2, 1};
 
-	if ( argc =! 2)
+	if (argc =! 2)
 	{
 		printf("Error\n");
 		return (1);
@@ -30,7 +30,7 @@ int main (int argc, char *argv[])
 		return (0);
 	}
 
-	for ( j = 0; j < 5 && num >= 0; j++)
+	for (j = 0; j < 5 && num >= 0; j++)
 	{
 		while (num >= coins[j])
 		{
