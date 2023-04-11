dc115daef14b10b01daa47ef8c26d18a7b2751f5
emilyakoth <emilyakoth9@gmail.com>
2023-04-11 11:18:53 -0700

    commit string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - copies a string
+ * @str: string to be copied
+ *
+ * Return: the string
+ */
+char *_strdup(char *str)
+{
+	int a = 0;
+	int i = 1;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i])
+	{
+		i++;
+	}
+
+	s = malloc((sizeof(char) * i) + 1);
+
+	if (s == NULL)
+		return (NULL);
+
+	while (a < i)
+	{
+		s[a] = str[a];
+		a++;
+	}
+	s[a] = ('\0');
+	return (s);
+}
