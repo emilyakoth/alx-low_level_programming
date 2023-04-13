f58d5e6625e5d51e9062fea24e499d6b6521ca19
emilyakoth <emilyakoth9@gmail.com>
2023-04-13 10:08:54 -0700

    malloc-fre

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory
+ * @b: size
+ *
+ * Return: Nothing
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *s
+	
+	s = malloc(b);
+
+	if (s == NULL)
+		exit (98);
+
+	return (s);
+}
