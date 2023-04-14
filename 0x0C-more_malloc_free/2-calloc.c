1e5677323677b6b16694097900035e5574ca8f7b
emilyakoth <emilyakoth9@gmail.com>
2023-04-13 19:15:20 -0700

    malloc-free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,11 +17,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 
 	l = nmemb * size;
-	p = malloc(1);
+	p = malloc(l);
 
 	if (p == NULL)
 		return (NULL);
-	
+
 	while (i < l)
 	{
 		p[i] = 0;
