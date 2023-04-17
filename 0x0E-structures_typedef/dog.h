6189ec681dc20b7ce9d2ca369e5e8f920d0ec4d9
emilyakoth <emilyakoth9@gmail.com>
2023-04-17 08:09:06 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+ * struct - struct dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Return:...
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
