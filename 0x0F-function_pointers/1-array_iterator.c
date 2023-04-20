b2c1b52891ff8a42640d00d0384e966bef6ff507
emilyakoth <emilyakoth9@gmail.com>
2023-04-19 18:56:35 -0700

    executes function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,11 +11,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int x = 0;
 
-	if (action != NULL && array != NULL & size > 0)
+	if (action != NULL && array != NULL && size > 0)
 	{
 		while (x < size)
 		{
-			ction(array[x]);
+			action(array[x]);
 			x++;
 		}
 	}
