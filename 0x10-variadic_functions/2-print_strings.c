fb65c4ca1410cf7bee0355873b9cc5e002670d89
emilyakoth <emilyakoth9@gmail.com>
2023-04-20 19:22:22 -0700

    variadic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	if (*separator == 0 || separator == NULL)
 		sep = "";
 	else 
-		sep = (char * separator)
+		sep = (char *) separator;
 	va_start(list, n);
 
 	if (n > 0)
