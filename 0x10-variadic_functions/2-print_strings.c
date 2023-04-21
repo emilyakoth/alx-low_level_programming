593d2b35725772fc32038c0083837226e6cb10b7
emilyakoth <emilyakoth9@gmail.com>
2023-04-20 19:17:09 -0700

    vatiaic functions

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - print strings
+ * @separator: space between
+ * @n: no of elements
+ *
+ * Return:
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	char *sep, *ptr;
+	unsigned int x;
+	va_list list;
+
+	if (*separator == 0 || separator == NULL)
+		sep = "";
+	else 
+		sep = (char * separator)
+	va_start(list, n);
+
+	if (n > 0)
+		printf("%s", va_arg(list, char *));
+	for (x = 1; x < n; x++)
+	{
+		ptr = va_arg(list, char*);
+		if (ptr == NULL)
+			ptr = "(nil)";
+		printf("%s%s", sep, ptr);
+	}
+	printf("/n");
+	va_end(list);
+}
