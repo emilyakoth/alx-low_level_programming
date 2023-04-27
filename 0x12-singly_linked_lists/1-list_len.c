7419e51b03913314ba5ee68fbac8065eb7ce4a2c
emilyakoth <emilyakoth9@gmail.com>
2023-04-27 15:48:36 -0700

    linked strings

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,13 +10,13 @@ size_t list_len(const list_t *h)
 {
 	const list_t *temp;
 	unsigned int counter = 0;
-	
+
 	temp = h;
-		while (temp)
+	while (temp)
 		{
 		counter++;
 		temp = temp->next;
 		}
-	return(counter);
+	return (counter);
 }
 
