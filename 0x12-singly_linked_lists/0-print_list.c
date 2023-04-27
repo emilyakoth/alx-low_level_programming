e8402fc372860d323a3a9cf888a13f65355bf8db
emilyakoth <emilyakoth9@gmail.com>
2023-04-27 15:37:46 -0700

    linked strings

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,7 +18,8 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
+		x++;
 	}
 
-	return (n);
+	return (x);
 }
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "lists.h"
 
 /**
  *  list_len - return elements to file
