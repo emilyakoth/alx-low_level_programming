c6a9af0689f91eb11387a2bc26fdb7480e3171e7
emilyakoth <emilyakoth9@gmail.com>
2023-04-27 17:18:50 -0700

    linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -11,5 +11,5 @@ void first(void) __attribute__ ((constructor));
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("bore my house upon my back!\n");
+	printf("I bore my house upon my back!\n");
 }
