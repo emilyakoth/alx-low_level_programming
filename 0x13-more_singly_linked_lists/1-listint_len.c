c157163500228630a26687169dea433f20af6880
emilyakoth <emilyakoth9@gmail.com>
2023-05-02 15:54:34 -0700

    More singly linked lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - retunr no of elements
+ * @h: linked list
+ *
+ * Return: no onf notes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t a = 0;
+
+	while (h)
+	{
+		a++;
+		h = h->next;
+	}
+
+	retunr (a);
+}
